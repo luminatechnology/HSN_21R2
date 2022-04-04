69645d99abdf729fa5c74c2f0541f132191400b8
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2022-04-04 22:13:21 +0800

    Chang the custom field from parent DAC to one of child DACs.

--- HSNFinance/HSNFinance/DAC_Extensions/APRegisterExtensions.cs
+++ HSNFinance/HSNFinance/DAC_Extensions/APRegisterExtensions.cs
@@ -4,14 +4,13 @@ using PX.Objects.GL;
 
 namespace PX.Objects.AP
 {
-    public class APRegisterExt : PXCacheExtension<PX.Objects.AP.APRegister>
+    public class APInvoiceExt2 : PXCacheExtension<PX.Objects.AP.APInvoice>
     {
         #region UsrInvoiceDate
         [PXDBDate()]
         [PXUIField(DisplayName = "Invoice Date")]
         [PXDefault()]
-        [PXFormula(typeof(IIf<Where<APRegister.origModule, In3<BatchModule.moduleEP, BatchModule.modulePO>>, APRegister.docDate, 
-                              IIf<Where<APPayment.docType, In3<APDocType.check, APDocType.refund, APDocType.voidCheck, APDocType.prepayment, APDocType.debitAdj, APDocType.voidRefund>>, APPayment.adjDate, Null>>))]
+        [PXFormula(typeof(IIf<Where<APRegister.origModule, In3<BatchModule.moduleEP, BatchModule.modulePO>>, APRegister.docDate, Null>))]
         public virtual DateTime? UsrInvoiceDate { get; set; }
         public abstract class usrInvoiceDate : PX.Data.BQL.BqlDateTime.Field<usrInvoiceDate> { }
         #endregion
