096f9a5456beb9dfc1aa95e424fd28cc90b8d521
WayneLin <wayne.lin@lumina-tech.com>
2022-06-17 15:01:02 +0800

    1.Fix ManufacturerModelCD string limit

---
+++ HSNCustomizations/HSNCustomizations/DAC_Extension/FSManufacturerModelExtension.cs
@@ -0,0 +1,16 @@
+﻿using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Objects.FS
+{
+    public class FSManufacturerModelExtension : PXCacheExtension<FSManufacturerModel>
+    {
+        [PXDBString(20, IsKey = true, IsUnicode = true, InputMask = ">CCCCCCCCCCCCCCCCCCCC")]
+        [PXMergeAttributes(Method = MergeMethod.Merge)]
+        public virtual string ManufacturerModelCD { get; set; }
+    }
+}
