c2e9c5856feda3e5c5195b423c8c3cfb76103a24
林子瑋 <JO3.JO3@hotmail.com>
2022-09-27 11:17:33 +0800

    1.HSN Customization - Enable the Default Value of the Tax Calculation Mode in Customer Location copied from Customer

---
+++ HSNCustomizations/HSNCustomizations/Graph_Extension/CustomerLocationMaint.cs
@@ -0,0 +1,30 @@
+﻿using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Objects.CR;
+
+namespace PX.Objects.AR
+{
+    public class CustomerLocationMaintExt : PXGraphExtension<CustomerLocationMaint>
+    {
+
+        #region Events
+        public virtual void _(Events.FieldDefaulting<Location.cTaxCalcMode> e, PXFieldDefaulting baseHandler)
+        {
+            baseHandler?.Invoke(e.Cache, e.Args);
+            var row = e.Row as Location;
+            if (row.BAccountID.HasValue)
+            {
+                var baccountInfo = BAccount.PK.Find(Base, row.BAccountID);
+                var locationInfo = Location.PK.Find(Base, baccountInfo.BAccountID, baccountInfo.DefLocationID);
+                e.NewValue = locationInfo?.CTaxCalcMode;
+            }
+        }
+
+        #endregion
+
+    }
+}
