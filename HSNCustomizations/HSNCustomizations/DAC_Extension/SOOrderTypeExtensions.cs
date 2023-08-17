fb637b703c9790f51d67a737c6aaaff655d7e40b
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-08-17 11:29:36 +0800

    Modified version after discussion with users at Joyce online meeting. Relaxation of validation condition for non-stock items.

--- HSNCustomizations/HSNCustomizations/DAC_Extension/SOOrderTypeExtensions.cs
+++ HSNCustomizations/HSNCustomizations/DAC_Extension/SOOrderTypeExtensions.cs
@@ -4,11 +4,11 @@ namespace PX.Objects.SO
 {
     public class SOOrderTypeExt : PXCacheExtension<SOOrderType>
     {
-        #region UsrRequireAtLeastOneSrvItemInSO
+        #region UsrRequireAtLeastOneNonStkItemInSO
         [PXDBBool()]
-        [PXUIField(DisplayName = "Require At Least One Service Item With Amount Greater Than 0 In Sales Order")]
-        public virtual bool? UsrRequireAtLeastOneSrvItemInSO { get; set; }
-        public abstract class usrRequireAtLeastOneSrvItemInSO : PX.Data.BQL.BqlBool.Field<usrRequireAtLeastOneSrvItemInSO> { }
+        [PXUIField(DisplayName = "Require At Least One Non-Stock Item With Amount Greater Than 0 In SO")]
+        public virtual bool? UsrRequireAtLeastOneNonStkItemInSO { get; set; }
+        public abstract class usrRequireAtLeastOneNonStkItemInSO : PX.Data.BQL.BqlBool.Field<usrRequireAtLeastOneNonStkItemInSO> { }
         #endregion
     }
 }
--- HSNCustomizations/HSNCustomizations/Graph_Extension/SOOrderEntry.cs
+++ HSNCustomizations/HSNCustomizations/Graph_Extension/SOOrderEntry.cs
@@ -7,8 +7,6 @@ using PX.Objects.AR;
 using PX.Objects.CS;
 using PX.Objects.CR;
 using PX.Objects.IN;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,58 +14,59 @@ namespace PX.Objects.SO
 {
     public class SOOrderEntryExt_HSN : PXGraphExtension<SOOrderEntry>
     {
-        public SelectFrom<Contact>
-                .InnerJoin<BAccount>.On<Contact.bAccountID.IsEqual<BAccount.bAccountID>>
-                .Where<Contact.bAccountID.IsEqual<SOOrder.customerID.FromCurrent>>.View StandardContactSelector;
+        public SelectFrom<Contact>.InnerJoin<BAccount>.On<Contact.bAccountID.IsEqual<BAccount.bAccountID>>
+                                  .Where<Contact.bAccountID.IsEqual<SOOrder.customerID.FromCurrent>>.View StandardContactSelector;
 
-        #region Delegate Actions
-        public delegate IEnumerable CreateShipmentIssueDelegate(PXAdapter adapter, [PXDate] DateTime? shipDate, [PXInt] int? siteID);
-        [PXOverride]
-        public IEnumerable CreateShipmentIssue(PXAdapter adapter, [PXDate] DateTime? shipDate, [PXInt] int? siteID, CreateShipmentIssueDelegate baseMethod)
-        {
-            ///<remarks>
-            /// The system should verify that the Details tab contains a minimum of one inventory item with its type (InventoryItem.ItemType) marked as "Service" 
-            /// and its amount is greater than 0 at the time the 'Create Shipment' action is executed. 
-            ///</remarks>
-            List<SOLine> lines = Base.Transactions.Select().RowCast<SOLine>().Where(w => InventoryItem.PK.Find(Base, w.InventoryID)?.ItemType == INItemTypes.ServiceItem).ToList();
-
-            if (Base.soordertype.Current?.GetExtension<SOOrderTypeExt>().UsrRequireAtLeastOneSrvItemInSO == true &&
-                lines.Exists(f => f.CuryLineAmt > 0m) == false)
-            {
-                throw new PXSetPropertyException<SOLine.inventoryID>(HSNMessages.SrvItemWithNoAmt);
-            }
-
-            return baseMethod(adapter, shipDate, siteID);
-        }
+        #region Cache Attached
+        [PXMergeAttributes(Method = MergeMethod.Replace)]
+        [PXUIEnabled(typeof(Where<SOOrder.customerID, IsNotNull>))]
+        [ContactRaw(typeof(SOOrder.customerID), null, null, null, 
+                    new[]
+                    {
+                        typeof(Contact.displayName),
+                        typeof(ContactExtensions.usrLocationID),
+                        typeof(Contact.salutation),
+                        typeof(Contact.fullName),
+                        typeof(BAccount.acctCD),
+                        typeof(Contact.eMail),
+                        typeof(Contact.phone1),
+                        typeof(Contact.contactType)
+                    }, 
+                    new string[]
+                    { 
+                        "Contact",
+                        "Location",
+                        "Job Title",
+                        "Account Name",
+                        "Business Account",
+                        "Email",
+                        "Phone GG",
+                        "Type"
+                    }, WithContactDefaultingByBAccount = true)]
+        public virtual void _(Events.CacheAttached<SOOrder.contactID> e) { }
         #endregion
 
         #region Events
-        [PXMergeAttributes(Method = MergeMethod.Replace)]
-        [PXUIEnabled(typeof(Where<SOOrder.customerID, IsNotNull>))]
-        [ContactRaw(typeof(SOOrder.customerID), null, null, null, new[]
+        protected void _(Events.RowPersisting<SOOrder> e, PXRowPersisting baseHandler)
         {
-            typeof(Contact.displayName),
-            typeof(ContactExtensions.usrLocationID),
-            typeof(Contact.salutation),
-            typeof(Contact.fullName),
-            typeof(BAccount.acctCD),
-            typeof(Contact.eMail),
-            typeof(Contact.phone1),
-            typeof(Contact.contactType)
-        }, new string[]
-        {
-            "Contact",
-            "Location",
-            "Job Title",
-            "Account Name",
-            "Business Account",
-            "Email",
-            "Phone GG",
-            "Type"
-        }, WithContactDefaultingByBAccount = true)]
-        public virtual void _(Events.CacheAttached<SOOrder.contactID> e) { }
+            baseHandler?.Invoke(e.Cache, e.Args);
 
-        public virtual void _(Events.FieldUpdated<SOOrder.customerID> e, PXFieldUpdated baseHandler)
+            if (Base.soordertype.Current?.GetExtension<SOOrderTypeExt>().UsrRequireAtLeastOneNonStkItemInSO == true)
+            {
+                ///<remarks>
+                /// The system should verify that the Details tab contains a minimum of one non-stock inventory item and its amount is greater than 0 at the time the 'Save' action is executed. 
+                ///</remarks>
+                List<SOLine> lines = Base.Transactions.Select().RowCast<SOLine>().Where(w => InventoryItem.PK.Find(Base, w.InventoryID)?.StkItem == false).ToList();
+                
+                var line = lines.Find(f => f.CuryLineAmt <= 0m);
+
+                if (line != null)
+                {
+                    Base.Transactions.Cache.RaiseExceptionHandling<SOLine.inventoryID>(line, null, new PXSetPropertyException(HSNMessages.NonStkItemWithNoAmt, PXErrorLevel.RowError));
+                }
+            }
+        }
+        public void _(Events.FieldUpdated<SOOrder.customerID> e, PXFieldUpdated baseHandler)
         {
             baseHandler?.Invoke(e.Cache, e.Args);
             var setup = SelectFrom<LUMHSNSetup>.View.Select(Base).TopFirst;
