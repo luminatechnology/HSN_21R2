c4f8319006dca961937061466776f10781941408
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-09-01 23:09:10 +0800

    Since journal transaction desc comes from FA which is cleared due to standard logic.

--- HSNFinance/HSNFinance/Graph_Extensions/JournalEntry.cs
+++ HSNFinance/HSNFinance/Graph_Extensions/JournalEntry.cs
@@ -10,19 +10,15 @@ namespace PX.Objects.GL
 {
     public class JournalEntry_Extensions : PXGraphExtension<JournalEntry>
     {
-        public const string PurchaseReceipt = "PO302000";
-        public const string Shipment        = "SO302000";
-
         #region Event Handlers
         protected void _(Events.RowPersisted<Batch> e)
         {
             // Since the description only needs to change when when it's null/blank and then add it to this trigger multiple event.
-            if (e.Operation != PXDBOperation.Delete && e.Row.Status == BatchStatus.Unposted && string.IsNullOrEmpty(e.Row.Description) )
+            if (e.Operation != PXDBOperation.Delete)
             {
                 UpdateBatchDesc(e.Row, Base.GLTranModuleBatNbr.Current?.RefNbr);
             }
         }
-
         protected void _(Events.RowUpdating<GLTran> e, PXRowUpdating baseHandler)
         {
             baseHandler?.Invoke(e.Cache, e.Args);
@@ -43,47 +39,67 @@ namespace PX.Objects.GL
         /// <param name="iNRefNbr"></param>
         public virtual void UpdateBatchDesc(Batch batch, string iNRefNbr)
         {
-            string description = null;
-
-            switch (batch.CreatedByScreenID)
+            if (batch.Status == BatchStatus.Unposted && string.IsNullOrEmpty(batch.Description))
             {
-                ///<remarks> 
-                /// After enabling the checkbox in Purchase Order Preferences, 
-                /// the Journal Transactions that triggered by Releasing the Purchase Receipts 
-                /// it will copy the Description (POOrder.OrderDesc) to Batch.Description (Inventory Receipts).
-                ///</remarks>
-                case PurchaseReceipt:
-                    if (SelectFrom<POSetup>.View.SelectSingleBound(Base, null).TopFirst?.GetExtension<POSetupExt>().UsrCopyHeaderDescFromPO == true)
-                    {
-                        description = SelectFrom<POOrder>.InnerJoin<POOrderReceipt>.On<POOrderReceipt.orderNoteID.IsEqual<POOrder.noteID>>
-                                                         .InnerJoin<INRegister>.On<INRegister.pOReceiptType.IsEqual<POOrderReceipt.receiptType>
-                                                                                  .And<INRegister.pOReceiptNbr.IsEqual<POOrderReceipt.receiptNbr>>>
-                                                         .Where<INRegister.refNbr.IsEqual<@P.AsString>>.View
-                                                         .SelectSingleBound(Base, null, iNRefNbr).TopFirst?.OrderDesc;
-                    }
-                    break;
-                ///<remarks>
-                /// After enabling the checkbox in Sales Order Preferences, 
-                /// the Journal Transactions that triggered by Releasing the Shipment or related Invoices,
-                /// it will copy the Description (SOOrder.OrderDesc) to Batch.Description (Inventory Issues).
-                /// </remarks>
-                case Shipment:
-                    if (SelectFrom<SOSetup>.View.SelectSingleBound(Base, null).TopFirst?.GetExtension<SOSetupExt>().UsrCopyHeaderDescFromSO == true)
-                    {
-                        description = SelectFrom<SOOrder>.InnerJoin<SOOrderShipment>.On<SOOrderShipment.orderNoteID.IsEqual<SOOrder.noteID>>
-                                                         .InnerJoin<INRegister>.On<INRegister.sOShipmentType.IsEqual<SOOrderShipment.shipmentType>
-                                                                                  .And<INRegister.sOShipmentNbr.IsEqual<SOOrderShipment.shipmentNbr>>>
-                                                         .Where<INRegister.refNbr.IsEqual<@P.AsString>>.View
-                                                         .SelectSingleBound(Base, null, iNRefNbr).TopFirst?.OrderDesc;
-                    }
-                    break;
-                default:
-                    description = batch.Module == BatchModule.FA ? "Monthly Depreciation" : batch.Module == BatchModule.DR ? "Deferred Revenue Recognition" : null;
-                    break;
-            }
+                const string PurchaseReceipt = "PO302000";
+                const string Shipment        = "SO302000";
+                
+                string description = null;
 
-            Base.BatchModule.Cache.SetValue<Batch.description>(batch, description);
-            Base.BatchModule.Cache.MarkUpdated(batch);
+                switch (batch.CreatedByScreenID)
+                {
+                    ///<remarks> 
+                    /// After enabling the checkbox in Purchase Order Preferences, 
+                    /// the Journal Transactions that triggered by Releasing the Purchase Receipts 
+                    /// it will copy the Description (POOrder.OrderDesc) to Batch.Description (Inventory Receipts).
+                    ///</remarks>
+                    case PurchaseReceipt:
+                        if (SelectFrom<POSetup>.View.SelectSingleBound(Base, null).TopFirst?.GetExtension<POSetupExt>().UsrCopyHeaderDescFromPO == true)
+                        {
+                            description = SelectFrom<POOrder>.InnerJoin<POOrderReceipt>.On<POOrderReceipt.orderNoteID.IsEqual<POOrder.noteID>>
+                                                             .InnerJoin<INRegister>.On<INRegister.pOReceiptType.IsEqual<POOrderReceipt.receiptType>
+                                                                                      .And<INRegister.pOReceiptNbr.IsEqual<POOrderReceipt.receiptNbr>>>
+                                                             .Where<INRegister.refNbr.IsEqual<@P.AsString>>.View
+                                                             .SelectSingleBound(Base, null, iNRefNbr).TopFirst?.OrderDesc;
+                        }
+                        break;
+                    ///<remarks>
+                    /// After enabling the checkbox in Sales Order Preferences, 
+                    /// the Journal Transactions that triggered by Releasing the Shipment or related Invoices,
+                    /// it will copy the Description (SOOrder.OrderDesc) to Batch.Description (Inventory Issues).
+                    /// </remarks>
+                    case Shipment:
+                        if (SelectFrom<SOSetup>.View.SelectSingleBound(Base, null).TopFirst?.GetExtension<SOSetupExt>().UsrCopyHeaderDescFromSO == true)
+                        {
+                            description = SelectFrom<SOOrder>.InnerJoin<SOOrderShipment>.On<SOOrderShipment.orderNoteID.IsEqual<SOOrder.noteID>>
+                                                             .InnerJoin<INRegister>.On<INRegister.sOShipmentType.IsEqual<SOOrderShipment.shipmentType>
+                                                                                      .And<INRegister.sOShipmentNbr.IsEqual<SOOrderShipment.shipmentNbr>>>
+                                                             .Where<INRegister.refNbr.IsEqual<@P.AsString>>.View
+                                                             .SelectSingleBound(Base, null, iNRefNbr).TopFirst?.OrderDesc;
+                        }
+                        break;
+                    default:
+                        description = batch.Module == BatchModule.FA ? "Monthly Depreciation" : batch.Module == BatchModule.DR ? "Deferred Revenue Recognition" : null;
+                        break;
+                }
+
+                if (!string.IsNullOrEmpty(description))
+                {
+                    if (batch.Module == BatchModule.FA)
+                    {
+                        PXUpdate<Set<Batch.description, Required<Batch.description>>,
+                                 Batch,
+                                 Where<Batch.module, Equal<Required<Batch.module>>,
+                                       And<Batch.batchNbr, Equal<Required<Batch.batchNbr>>>>>
+                                .Update(Base, description, batch.Module, batch.BatchNbr);
+                    }
+                    else
+                    {
+                        Base.BatchModule.Cache.SetValue<Batch.description>(batch, description);
+                        Base.BatchModule.Cache.MarkUpdated(batch);
+                    }
+                }
+            }
         }
         #endregion
     }
