7186a7e3d92b7a9079e5fa23dbc28cd51b9e5e76
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-09-25 14:25:26 +0800

    Added a DAC that controls the description field.

---
+++ HSNFinance/HSNFinance/DAC_Extensions/ARPaymentExt_Finance.cs
@@ -0,0 +1,33 @@
+﻿using PX.Data;
+using PX.Data.BQL.Fluent;
+using PX.Objects.SO;
+
+namespace PX.Objects.AR
+{
+    [PXNonInstantiatedExtension()]
+    public class ARPaymentExt_ExistingColumn : PXCacheExtension<ARPayment>
+    {
+        #region DocDesc
+        [PXMergeAttributes(Method = MergeMethod.Append)]
+        [PXDefault()]
+        [PXUIRequired(typeof(Where<ARPaymentExt_Finance.usrIsRequireHeaderDesc, Equal<True>>))]
+        public virtual string DocDesc { get; set; }
+        #endregion
+    }
+
+    public class ARPaymentExt_Finance : PXCacheExtension<ARPayment>
+    {
+        #region Unbound Fields
+
+        #region UsrIsRequireHeaderDesc
+        [PXBool()]
+        [PXDefault(typeof(SelectFrom<SOSetup>.SearchFor<SOSetupExt.usrReqHeaderDescInSO>),
+                   PersistingCheck = PXPersistingCheck.Nothing)]
+        [PXDBScalar(typeof(SelectFrom<SOSetup>.SearchFor<SOSetupExt.usrReqHeaderDescInSO>))]
+        public virtual bool? UsrIsRequireHeaderDesc { get; set; }
+        public abstract class usrIsRequireHeaderDesc : PX.Data.BQL.BqlBool.Field<usrIsRequireHeaderDesc> { }
+        #endregion
+
+        #endregion
+    }
+}
