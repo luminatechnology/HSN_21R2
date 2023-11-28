b3f84e713e546c88d4b602ebc9d5429b08325f32
林子瑋 <jo3.jo3@hotmail.com>
2023-11-28 09:48:51 +0800

    1.HSN Finance - 完成Create Debit memo after release cash sale

--- HSNFinance/HSNFinance/Graph_Extensions/SOInvoiceEntry_Finance.cs
+++ HSNFinance/HSNFinance/Graph_Extensions/SOInvoiceEntry_Finance.cs
@@ -1,7 +1,9 @@
-﻿using PX.Data;
+﻿using HSNCustomizations.DAC;
+using PX.Data;
 using PX.Data.BQL;
 using PX.Data.BQL.Fluent;
 using PX.Objects.AR;
+using PX.Objects.FS;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,6 +20,7 @@ namespace PX.Objects.SO
         [PXOverride]
         public virtual IEnumerable Release(PXAdapter adapter, ReleaseDelegate baseMethod)
         {
+            var preference = SelectFrom<LUMHSNSetup>.View.Select(Base).TopFirst;
             var doc = Base.Document.Current;
             var releaseResult = baseMethod.Invoke(adapter);
             /// HSNM：Add a Process ‘Process Open Prepayments for Cash Sales’
@@ -30,8 +33,8 @@ namespace PX.Objects.SO
                                  .InnerJoin<ARInvoice>.On<ARTran.refNbr.IsEqual<ARInvoice.refNbr>
                                                      .And<ARInvoice.docType.IsEqual<P.AsString>>>
                                  .Where<ARTran.tranDesc.IsEqual<P.AsString>>
-                                 .View.Select(Base,ARDocType.CashSale, prepayment?.AdjgRefNbr).TopFirst;
-                    if(mapARTran != null)
+                                 .View.Select(Base, ARDocType.CashSale, prepayment?.AdjgRefNbr).TopFirst;
+                    if (mapARTran != null)
                     {
                         PXDatabase.Update<ARTran>(
                             new PXDataFieldAssign<ARTranExt_Finance.usrSettled>(true),
@@ -41,6 +44,83 @@ namespace PX.Objects.SO
                     }
                 }
             }
+
+            #region [HSNM] Process Open Prepayments for Cash Sales
+            // Cash Sales Mapping Service Order
+            if (preference?.EnableChgInvTypeOnBill ?? false)
+            {
+                var mapFSARTran = SelectFrom<FSARTran>
+                                 .Where<FSARTran.tranType.IsEqual<P.AsString>
+                                   .And<FSARTran.refNbr.IsEqual<P.AsString>>>
+                                 .View.Select(Base, ARDocType.CashSale, doc?.RefNbr).TopFirst;
+                if (mapFSARTran != null && doc.DocType == ARDocType.CashSale)
+                {
+                    try
+                    {
+                        // Service Order/Appointment Mapping Prepayment
+                        var mapFSAdjust = SelectFrom<FSAdjust>
+                                         .Where<FSAdjust.adjdOrderNbr.IsEqual<P.AsString>
+                                           .And<FSAdjust.adjgDocType.IsEqual<P.AsString>>>
+                                         .View.Select(Base, mapFSARTran?.ServiceOrderRefNbr, ARDocType.Prepayment).RowCast<FSAdjust>();
+
+                        // Create Debit Memo
+                        var soInvEntry = PXGraph.CreateInstance<SOInvoiceEntry>();
+                        var arPaymentEntry = PXGraph.CreateInstance<ARPaymentEntry>();
+
+                        #region Debit memo Header
+                        var debitDoc = soInvEntry.Document.Cache.CreateInstance() as ARInvoice;
+                        debitDoc.DocType = "DRM";
+                        debitDoc = soInvEntry.Document.Insert(debitDoc);
+                        debitDoc.Status = ARDocStatus.Hold;
+                        debitDoc.CustomerID = Base.Document.Current?.CustomerID;
+                        debitDoc.DocDesc = "Settle Open Prepayment";
+                        soInvEntry.Document.Update(debitDoc);
+                        #endregion
+
+                        #region Debit memo Details & Applications
+                        foreach (var item in mapFSAdjust)
+                        {
+                            arPaymentEntry.Document.Current = arPaymentEntry.Document.Search<ARPayment.refNbr>(item?.AdjgRefNbr, "PPM");
+
+                            #region Details
+                            var line = soInvEntry.Transactions.Cache.CreateInstance() as ARTran;
+                            line.BranchID = arPaymentEntry.Document.Current?.BranchID;
+                            line.TranDesc = "Settle Open Prepayment";
+                            line.CuryExtPrice = arPaymentEntry.Document.Current?.CuryUnappliedBal;
+                            line.AccountID = arPaymentEntry.Document.Current?.ARAccountID;
+                            line.SubID = arPaymentEntry.Document.Current?.ARSubID;
+                            line = soInvEntry.Transactions.Insert(line);
+                            #endregion
+
+                            #region Applications
+                            var appt = soInvEntry.Adjustments.Cache.CreateInstance() as ARAdjust2;
+                            appt.AdjgDocType = ARPaymentType.Prepayment;
+                            appt.AdjgRefNbr = arPaymentEntry.Document.Current?.RefNbr;
+                            appt.CuryAdjdAmt = arPaymentEntry.Document.Current?.CuryUnappliedBal;
+                            appt = soInvEntry.Adjustments.Insert(appt);
+                            #endregion
+                        }
+                        #endregion
+
+                        // Save Invoice
+                        soInvEntry.Save.Press();
+                        soInvEntry.releaseFromHold.Press();
+                        //throw new PXRedirectRequiredException(soInvEntry, true, "Invoice");
+                    }
+                    catch (PXRedirectRequiredException er)
+                    {
+                        throw er;
+                    }
+                    catch (Exception ex)
+                    {
+                        PXTrace.WriteError($"Create Debit memo failed : {ex.Message}");
+                    }
+
+                }
+            }
+
+            #endregion
+
             return releaseResult;
         }
         #endregion
