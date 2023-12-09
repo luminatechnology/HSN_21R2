2093ba9717d8989021f2ed1f45ab953f03f6ae8b
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-12-09 22:54:10 +0800

    Test merge diff

--- HSNCustomizations/HSNCustomizations/Graph_Extension/SM_SOOrderEntry.cs
+++ HSNCustomizations/HSNCustomizations/Graph_Extension/SM_SOOrderEntry.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PX.Objects.SO;
 using PX.Objects.PM;
 using PX.Objects.CS;
